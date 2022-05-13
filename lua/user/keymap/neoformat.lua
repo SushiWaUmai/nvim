0bd7d0f42db6c0e5a0dfc76a75dbadeb5514cde4
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-14 01:13:14 +0200

    feat: :sparkles: update neoformat command keymap to alt-shift-f

--- lua/user/keymap/neoformat.lua
+++ lua/user/keymap/neoformat.lua
@@ -3,4 +3,5 @@ local keymap_opts = require("user.keymap.utils")
 local keymap = keymap_opts.keymap
 local opts = keymap_opts.opts
 
-keymap("n", "<C-i>", "<cmd>Neoformat<CR>", opts)
+keymap("n", "<A-F>", "<cmd>Neoformat<CR>", opts)
+
