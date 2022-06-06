4c7f72017a1950a1439a24082f2df84d94a99706
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-06 15:40:22 +0200

    style: remove shading_factor from toggleterm

--- lua/config/immediate/toggleterm.lua
+++ lua/config/immediate/toggleterm.lua
@@ -11,7 +11,6 @@ toggleterm.setup({
 	hide_numbers = true,
 	shade_filetypes = {},
 	shade_terminals = true,
-	shading_factor = 2,
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
