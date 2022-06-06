6ef0975beda17aee3047017f0c7f9b559393a2b6
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-06 16:02:34 +0200

    fix: :bug: prevent treesitter from downloading languages on startup

--- lua/config/deferred/treesitter.lua
+++ lua/config/deferred/treesitter.lua
@@ -7,41 +7,10 @@ local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
 ft_to_parser.motoko = "typescript"
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- List of parsers to ignore installing
+	sync_install = true,
 	highlight = {
-		-- use_languagetree = true,
-		enable = true, -- false will disable the whole extension
-		-- disable = { "css", "html" }, -- list of language that will be disabled
-		disable = { "css", "markdown" }, -- list of language that will be disabled
+		enable = true,
 		additional_vim_regex_highlighting = true,
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-	autotag = {
-		enable = true,
-		disable = { "xml" },
-	},
-	rainbow = {
-		enable = true,
-		colors = {
-			"Gold",
-			"Orchid",
-			"DodgerBlue",
-			-- "Cornsilk",
-			-- "Salmon",
-			-- "LawnGreen",
-		},
-		disable = { "html" },
-	},
-	playground = {
-		enable = true,
+		use_languagetree = true,
 	},
 })
