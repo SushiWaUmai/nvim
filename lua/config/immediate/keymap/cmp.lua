07814c855d913187fb0bccc223f487a301118552
SushiWaUmai <eugene.bb1@gmail.com>
2022-06-07 19:09:51 +0200

    feat: :sparkles: update cmp keybindings

--- lua/config/immediate/keymap/cmp.lua
+++ lua/config/immediate/keymap/cmp.lua
@@ -4,15 +4,11 @@ if not cmp_status_ok then
 end
 
 local mapping = {
-	["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-	["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-	["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-	["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-	["<C-e>"] = cmp.mapping({
-		i = cmp.mapping.abort(),
-		c = cmp.mapping.close(),
-	}),
-	["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	["<C-f>"] = cmp.mapping.scroll_docs(4),
+	["<C-Space>"] = cmp.mapping.complete(),
+	["<C-e>"] = cmp.mapping.abort(),
+	["<CR>"] = cmp.mapping.confirm({ select = true }),
 }
 
 return mapping
