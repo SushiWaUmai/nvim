d44dbec95b10a3d45c5bc7d4ed65164974e3f06d
Eugene Matsumura <eugene.bb1@gmail.com>
2022-09-19 19:56:27 +0200

    feat: :sparkles: update mason lsp configs to be customizable using files

--- lua/config/deferred/lsp/init.lua
+++ lua/config/deferred/lsp/init.lua
@@ -7,6 +7,11 @@ if not mason_lspconfig_status_ok then
   return
 end
 
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
 local lsp_keymap_status_ok, lsp_keymap = pcall(require, "config.immediate.keymap.lsp")
 if not lsp_keymap_status_ok then
 	return
@@ -20,22 +25,22 @@ end
 mason_lspconfig.setup()
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
--- mason.on_server_ready(function(server)
--- 	local opts = {
--- 		on_attach = lsp_keymap.on_attach,
--- 		flags = {
--- 			debounce_text_changes = 150,
--- 		},
--- 	}
+for _, server in pairs(mason_lspconfig.get_installed_servers()) do
+	local opts = {
+		on_attach = lsp_keymap.on_attach,
+		flags = {
+			debounce_text_changes = 150,
+		},
+	}
 
--- 	local lang_opts_status_ok, lang_opts = pcall(require, "config.deferred.lsp.settings." .. server.name)
--- 	if lang_opts_status_ok then
--- 		opts = vim.tbl_deep_extend("force", lang_opts, opts)
--- 	end
+	local lang_opts_status_ok, lang_opts = pcall(require, "config.deferred.lsp.settings." .. server)
+	if lang_opts_status_ok then
+		opts = vim.tbl_deep_extend("force", lang_opts, opts)
+	end
 
--- 	opts = vim.tbl_deep_extend("force", cmp_capabilities, opts)
+	opts = vim.tbl_deep_extend("force", cmp_capabilities, opts)
 
--- 	server:setup(opts)
--- end)
+  lspconfig[server].setup(opts)
+end
 
 lsp_sig.setup_signature()
