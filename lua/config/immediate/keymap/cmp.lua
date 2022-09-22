37465d6e3df974b70965eebaa6cb9860c73a4f39
Eugene Matsumura <eugene.bb1@gmail.com>
2022-09-22 15:08:30 +0200

    feat: :sparkles: add selectable complete item

--- lua/config/immediate/keymap/cmp.lua
+++ lua/config/immediate/keymap/cmp.lua
@@ -9,6 +9,32 @@ local mapping = {
 	["<C-Space>"] = cmp.mapping.complete(),
 	["<C-e>"] = cmp.mapping.abort(),
 	["<Tab>"] = cmp.mapping.confirm({ select = true }),
+	["<Down>"] = cmp.mapping(function(fallback)
+		if vim.fn.wildmenumode() == 1 then
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, true, true), "n", true)
+		elseif cmp.visible() then
+			cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+		else
+			fallback()
+		end
+	end, {
+		"i",
+		"s",
+		"c",
+	}),
+	["<Up>"] = cmp.mapping(function(fallback)
+		if vim.fn.wildmenumode() == 1 then
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Left>", true, true, true), "n", true)
+		elseif cmp.visible() then
+			cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+		else
+			fallback()
+		end
+	end, {
+		"i",
+		"s",
+		"c",
+	}),
 }
 
 return mapping
