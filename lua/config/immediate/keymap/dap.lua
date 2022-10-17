06d085a81d0d8aa6a4bcfd0e3a81aa72a2577ada
Eugene Matsumura <eugene.bb1@gmail.com>
2022-10-17 14:08:11 +0200

    feat: :art: rebind dap keybindings

--- lua/config/immediate/keymap/dap.lua
+++ lua/config/immediate/keymap/dap.lua
@@ -4,8 +4,8 @@ local keymap = keymap_opts.keymap
 local opts = keymap_opts.opts
 
 -- DAP
-keymap("n", "<F5>", "<cmd>lua require('dap').continue()<CR>", opts)
+keymap("n", "<A-r>", "<cmd>lua require('dap').continue()<CR>", opts)
 keymap("n", "<F10>", "<cmd>lua require('dap').step_over()<CR>", opts)
-keymap("n", "<F11>", "<cmd>lua require('dap').step_into()<CR>", opts)
-keymap("n", "<F12>", "<cmd>lua require('dap').step_out()<CR>", opts)
+keymap("n", "<A-i>", "<cmd>lua require('dap').step_into()<CR>", opts)
+keymap("n", "<A-o>", "<cmd>lua require('dap').step_out()<CR>", opts)
 keymap("n", "<A-b>", "<cmd>lua require('dap').toggle_breakpoint()<CR>", opts)
