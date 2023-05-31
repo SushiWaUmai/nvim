95177496e74a24a4ab78cf523a7b6b350e80a7d5
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-31 19:53:14 +0200

    feat: use tabs instead of spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,39 +1,39 @@
 local options = {
-	backup = false,
-	clipboard = "unnamedplus",
-	cmdheight = 2,
-	conceallevel = 0,
-	colorcolumn = "120",
-	fileencoding = "utf-8",
-	ignorecase = true,
-	mouse = "a",
-	pumheight = 10,
-	showmode = true,
-	showtabline = 2,
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	termguicolors = true,
-	timeoutlen = 100,
-	undofile = true,
-	updatetime = 300,
-	writebackup = false,
-	expandtab = true,
-	cursorline = true,
-	number = true,
-	relativenumber = true,
-	shiftwidth = 2,
-	tabstop = 2,
-	signcolumn = "yes",
-	wrap = false,
-	guifont = "MesloLGS Nerd Font Mono:h12",
-	confirm = true,
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  conceallevel = 0,
+  colorcolumn = "120",
+  fileencoding = "utf-8",
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,
+  showmode = true,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  termguicolors = true,
+  timeoutlen = 100,
+  undofile = true,
+  updatetime = 300,
+  writebackup = false,
+  expandtab = false,
+  cursorline = true,
+  number = true,
+  relativenumber = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  signcolumn = "yes",
+  wrap = false,
+  guifont = "MesloLGS Nerd Font Mono:h12",
+  confirm = true,
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 vim.opt.shortmess:append("c")
@@ -43,89 +43,89 @@ vim.cmd([[set autoindent]])
 -- shamelessly stolen from https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- This file is automatically loaded by plugins.init
 local function augroup(name)
-	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-	group = augroup("checktime"),
-	command = "checktime",
+  group = augroup("checktime"),
+  command = "checktime",
 })
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = augroup("highlight_yank"),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-	group = augroup("resize_splits"),
-	callback = function()
-		vim.cmd("tabdo wincmd =")
-	end,
+  group = augroup("resize_splits"),
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
 })
 
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd("BufReadPost", {
-	group = augroup("last_loc"),
-	callback = function()
-		local mark = vim.api.nvim_buf_get_mark(0, '"')
-		local lcount = vim.api.nvim_buf_line_count(0)
-		if mark[1] > 0 and mark[1] <= lcount then
-			pcall(vim.api.nvim_win_set_cursor, 0, mark)
-		end
-	end,
+  group = augroup("last_loc"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-	group = augroup("close_with_q"),
-	pattern = {
-		"PlenaryTestPopup",
-		"help",
-		"lspinfo",
-		"man",
-		"notify",
-		"qf",
-		"spectre_panel",
-		"startuptime",
-		"tsplayground",
-		"checkhealth",
-	},
-	callback = function(event)
-		vim.bo[event.buf].buflisted = false
-		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-	end,
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "checkhealth",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
 })
 
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
-	group = augroup("wrap_spell"),
-	pattern = { "gitcommit", "markdown" },
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
-	end,
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
 })
 
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	group = augroup("auto_create_dir"),
-	callback = function(event)
-		if event.match:match("^%w%w+://") then
-			return
-		end
-		local file = vim.loop.fs_realpath(event.match) or event.match
-		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-	end,
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
 })
 
 
 local file_exists = require("config.utils").file_exists
 
 if file_exists("~/.cache/calendar.vim/credentials.vim") then
-	vim.cmd([[source ~/.cache/calendar.vim/credentials.vim]])
+  vim.cmd([[source ~/.cache/calendar.vim/credentials.vim]])
 end
